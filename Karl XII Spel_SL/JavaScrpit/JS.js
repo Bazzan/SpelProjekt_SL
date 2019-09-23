4c1d69a8de0709592d105188f8fb003ec3a649b5
Bazzan <sebake01@gmail.com>
2019-09-23 15:10:36 +0200

    StartSida + SpelSida

---
+++ Karl XII Spel_SL/JavaScrpit/JS.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+
+
+
+    var backgorundImg = []
+    var imgCounter = 0;
+
+
+
+changeImg() = function(){
+    var pictureImg = backgorundImg[imgCounter];
+    document.body.style.backgroundImage = "url('"+pictureImg+"')";
+    imgCounter++;
+}
+
+
+}

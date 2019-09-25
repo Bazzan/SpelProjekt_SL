e08c42866d9fc747605497487a6a12525ca23f5f
Bazzan <sebake01@gmail.com>
2019-09-25 18:56:55 +0200

    Knapp och syntax, samt beskrivning
    jag har kommenterat ut ett fält vid knappen i spel dokumentet där jag förklara hur man gör för att göra knapparna till planeterna

--- Karl XII Spel_SL/JavaScrpit/JS.js
+++ Karl XII Spel_SL/JavaScrpit/JS.js
@@ -1,16 +1,16 @@
-window.onload = function(){
+// window.onload = function(){
 
 
-    var backgorundImg = []
-    var imgCounter = 0;
+//     var backgorundImg = []
+//     var imgCounter = 0;
 
 
 
-changeImg() = function(){
-    var pictureImg = backgorundImg[imgCounter];
-    document.body.style.backgroundImage = "url('"+pictureImg+"')";
-    imgCounter++;
-}
+// changeImg() = function(){
+//     var pictureImg = backgorundImg[imgCounter];
+//     document.body.style.backgroundImage = "url('"+pictureImg+"')";
+//     imgCounter++;
+// }
 
 
-}
+// }
